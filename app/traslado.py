e0104cfe665b28a332b298580d8f95bb9bc86505
MartinCastroMorales-mcm <mcm.martincastromorales@gmail.com>
2024-02-23 16:29:17 -0300

    final dia 23 avance en pdf

--- app/traslado.py
+++ app/traslado.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, url_for, redirect, flash
+from flask import Blueprint, render_template, request, url_for, redirect, flash, make_response
 from db import mysql
-
+from fpdf import FPDF
 traslado = Blueprint("traslado", __name__, template_folder="app/templates")
 
 
@@ -149,11 +149,15 @@ def delete_traslado(id):
                     """, (id,))
         traslaciones = cur.fetchall()
         for traslacion in traslaciones:
+            print("###############")
+            print(trasladoABorrar)
+            print("###############")
+            print(traslacion)
             cur.execute("""
                         UPDATE equipo
                         SET idUnidad = %s
                         WHERE idEquipo = %s 
-                        """, (trasladoABorrar[3], traslacion[1]))
+                        """, (trasladoABorrar[0]['idUnidadOrigen'], traslacion['idEquipo']))
         
         cur.execute("""DELETE 
                         FROM traslacion
@@ -211,5 +215,79 @@ def create_traslado(origen):
             mysql.connection.commit()
         
         flash("traslado agregado correctamente")
+
         return redirect(url_for('traslado.Traslado'))
     return redirect(url_for('traslado.Traslado'))
+
+
+@traslado.route("/traslado/create_pdf")
+def create_pdf():
+    #(Orientacion, unidades, formato)
+    #Orientacion P(portrait) o L(landscape)
+    class PDF(FPDF):
+        def header(self):
+            #logo
+            #imageUrl = url_for('static', filename='img/logo_junji.png')
+            #print(imageUrl)
+            self.image('logo_junji.png', 10, 8, 25)
+            #font
+            self.set_font('times', 'B', 12)
+            self.set_text_color(170,170,170)
+            #Title
+            self.cell(0, 30, '', border=False, ln=1, align='L')
+            self.cell(0, 5, 'JUNTA NACIONAL', border=False, ln=1, align='L')
+            self.cell(0, 5, 'INFANTILES', border=False, ln=1, align='L')
+            self.cell(0, 5, 'Unidad de Inventarios', border=False, ln=1, align='L')
+            #line break
+            self.ln(10)
+        
+        def footer(self):
+            self.set_y(-30)
+            self.set_font('times', 'B', 12)
+            self.set_text_color(170,170,170)
+            self.image('logo_inferior.jpg', 10, 8, 25)
+            self.cell(0,0, "", ln=1)
+            self.cell(0,0, "Junta Nacional de Jardines Infantiles-JUNJI", ln=1)
+            self.cell(0,5, "OHiggins Poniente 77 Concepción. 041-2125541", ln=1) #problema con el caracter ’
+            self.cell(0,5, "www.junji.cl", ln=1)
+
+
+
+    pdf = PDF('P', 'mm', 'A4')
+
+    pdf.add_page()
+
+
+    titulo = "ACTA DE TRASLADO N° 'n'"
+    parrafo_1 = "En Concepción 'fecha' se procede al traslado de bienes JUNJI de registro inventario desde 'Origen' hasta 'Destino' el siguiente detalle: "
+    TABLE_DATA = (
+    ("N°", "Articulos", "Serie", "Código Inventario", "Estado"),
+    ("1", "EpsonI5190", "X5NS117668", "8042812", "MAL"),
+    ("2", "EpsonI5190", "X5NS117668", "8042813", "MAL"),
+    ("3", "EpsonI5190", "X5NS117668", "8042814", "MAL"),
+    ("4", "EpsonI5190", "X5NS117668", "8042815", "MAL"),
+    )
+                
+    pdf.set_font('times', '', 20)
+    pdf.cell(0, 10, titulo, ln=True, align='C')            
+            
+    pdf.set_font('times', '', 12)
+    pdf.multi_cell(0, 10, parrafo_1)
+    with pdf.table() as table:
+        for datarow in TABLE_DATA:
+            row = table.row()
+            for datum in datarow:
+                row.cell(datum)
+    parrafo_2 = "Se ecuentran X en Bien, Y Regular y Z Mal"
+    pdf.multi_cell(0,10,parrafo_2, ln=True)
+    parrafo_3 = "Nombre Encargado:____________________"
+    pdf.multi_cell(0,10,parrafo_3, ln=True)
+    parrafo_4 = "Numero de RUT:____________________"
+    pdf.multi_cell(0,10,parrafo_4, ln=True)
+    parrafo_5 = "Nombre Ministro de Fe:____________________"
+    pdf.multi_cell(0,10,parrafo_5, ln=True)
+    parrafo_6 = "Firma:____________________"
+    pdf.multi_cell(0,10,parrafo_6, ln=True)
+    pdf.output('pdf_1.pdf')
+    return redirect(url_for('traslado.Traslado'))
+
