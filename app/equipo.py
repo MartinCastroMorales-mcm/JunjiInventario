869fc6df46b057fa611ed0f36822f4e8dcdac780
MartinCastroMorales-mcm <mcm.martincastromorales@gmail.com>
2024-03-05 14:17:01 -0300

    bug en equipo

--- app/equipo.py
+++ app/equipo.py
@@ -19,13 +19,16 @@ def Equipo(page=1):
     cur.execute(
         """ 
     SELECT e.idEquipo, e.Cod_inventarioEquipo, e.Num_serieEquipo, e.ObservacionEquipo, e.codigoproveedor_equipo, e.macEquipo, e.imeiEquipo, e.numerotelefonicoEquipo,e.idTipo_Equipo ,e.idEstado_Equipo, e.idUnidad, e.idOrden_compra, e.idModelo_equipo,te.idTipo_equipo, te.nombreidTipoequipo, ee.idEstado_equipo, ee.nombreEstado_equipo, u.idUnidad, u.nombreUnidad, oc.idOrden_compra, oc.nombreOrden_compra,
-    moe.idModelo_equipo, moe.nombreModeloequipo
+    moe.idModelo_equipo, moe.nombreModeloequipo, f.nombreFuncionario
     FROM equipo e
     INNER JOIN tipo_equipo te on te.idTipo_equipo = e.idTipo_Equipo
     INNER JOIN estado_equipo ee on ee.idEstado_equipo = e.idEstado_Equipo
     INNER JOIN Unidad u on u.idUnidad = e.idUnidad
     INNER JOIN orden_compra oc on oc.idOrden_compra = e.idOrden_compra
     INNER JOIN modelo_equipo moe on moe.idModelo_Equipo = e.idModelo_equipo
+    INNER JOIN equipo_asignacion ea on ea.idEquipo = e.idEquipo
+    INNER JOIN asignacion a on a.idAsignacion = ea.idAsignacion
+    INNER JOIN funcionario f on f.rutFuncionario = a.rutFuncionario
     LIMIT {} OFFSET {}
     """.format(
             perpage, offset
@@ -326,12 +329,23 @@ def mostrar_asociados_unidad(idUnidad, page=1):
 @equipo.route("/mostrar_asociados_funcionario/<rutFuncionario>")
 @equipo.route("/mostrar_asociados_funcionario/<rutFuncionario>/<page>")
 def mostrar_asociados_funcionario(rutFuncionario, page=1):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM tipo_equipo")
+    tipoe_data = cur.fetchall()
+    cur.execute("SELECT idEstado_equipo, nombreEstado_equipo FROM estado_equipo")
+    estadoe_data = cur.fetchall()
+    cur.execute("SELECT idUnidad, nombreUnidad FROM Unidad")
+    ubi_data = cur.fetchall()
+    cur.execute("SELECT idOrden_compra, nombreOrden_compra FROM orden_compra")
+    ordenc_data = cur.fetchall()
+    cur.execute("SELECT idModelo_Equipo, nombreModeloequipo FROM modelo_equipo")
+    modeloe_data = cur.fetchall()
+
     page = int(page)
     page = 1
     perpage = 200 #porque ningun funcionario tendra tantos 
                     #equipos que la paginacion sea nesesaria
     offset = (page - 1) * perpage
-    cur = mysql.connection.cursor()
     cur.execute("SELECT COUNT(*) FROM EQUIPO")
     total = cur.fetchone()
     total = int(str(total).split(":")[1].split("}")[0])
@@ -343,6 +357,21 @@ def mostrar_asociados_funcionario(rutFuncionario, page=1):
                 ORDER BY a.fecha_inicioAsignacion DESC
                 """, (rutFuncionario,))
     asignaciones = cur.fetchall()
+    print("########################")
+    print(asignaciones)
+    if(len(asignaciones) == 0):
+        
+        return render_template(
+            "equipo.html",
+            equipo=(),
+            tipo_equipo=tipoe_data,
+            estado_equipo=estadoe_data,
+            orden_compra=ordenc_data,
+            Unidad=ubi_data,
+            modelo_equipo=modeloe_data,
+            page=page,
+            lastpage=False,
+        )
     ultimaAsignacion = asignaciones[0]
     primeraAsignacion = asignaciones[-1] #-1 deberia dar el ultimo elemento
 
@@ -375,16 +404,6 @@ def mostrar_asociados_funcionario(rutFuncionario, page=1):
         ),
     )
     data = cur.fetchall()
-    cur.execute("SELECT * FROM tipo_equipo")
-    tipoe_data = cur.fetchall()
-    cur.execute("SELECT idEstado_equipo, nombreEstado_equipo FROM estado_equipo")
-    estadoe_data = cur.fetchall()
-    cur.execute("SELECT idUnidad, nombreUnidad FROM Unidad")
-    ubi_data = cur.fetchall()
-    cur.execute("SELECT idOrden_compra, nombreOrden_compra FROM orden_compra")
-    ordenc_data = cur.fetchall()
-    cur.execute("SELECT idModelo_Equipo, nombreModeloequipo FROM modelo_equipo")
-    modeloe_data = cur.fetchall()
     return render_template(
         "equipo.html",
         equipo=data,
