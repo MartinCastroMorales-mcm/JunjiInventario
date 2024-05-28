eaa37cabe516b667d91be113a5ebce74e83c81aa
MartinCastroMorales-mcm <mcm.martincastromorales@gmail.com>
2024-05-28 11:32:16 -0400

    production2?

--- app/main.py
+++ app/main.py
@@ -43,4 +43,4 @@ app.register_blueprint(cuentas)
 # se inicia la aplicacion, y confirma que __name__ sea la aplicacion main y no un modulo
 if __name__ == "__main__":
     #seleccion de puerto y debug para no tener que iniciar la aplicacion cada vez que se realicen cambios
-    app.run(port=3000, debug=True)
+    app.run(host='0.0.0.0', port=3000)
