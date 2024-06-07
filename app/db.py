b1b922c1996999c7294ccc75ac8cd0ca615aa3ef
Martin Castro Morales <martin@debian>
2024-06-07 16:49:50 -0400

    entornos de variables

--- app/db.py
+++ app/db.py
@@ -1,21 +1,22 @@
 from app import app
 from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
+from env_vars import cuentas
 
 import os
 
 
 # Configuramos la conexion a base de datos
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or 'root'
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or ''
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or cuentas['username']
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or cuentas['contrasenna']
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') or 'localhost' # localhost
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') or 'inventariofinal'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # Print or log the environment variables
 
-MYSQL_USER = app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or 'root'
-MYSQL_PASSWORD = app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or ''
+MYSQL_USER = app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or cuentas['username']
+MYSQL_PASSWORD = app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or cuentas['contrasenna']
 MYSQL_HOST = app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') or 'localhost' # localhost
 MYSQL_DB = app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') or 'inventariofinal'
 
---
+++ app/env_vars.py
@@ -0,0 +1,16 @@
+paths = {
+    "win_path": f"\path",
+    "linux_path": f"/path"
+}
+cuentas = {
+    "nombre_usuario": "martin",
+    "contrasenna": "",
+    "nombre_correo": "martin.castro2101@alumnos.ubiobio.cl",
+    "correo_contrasenna": "1234"
+}
+#cuenta servidor
+#nombre_usuario : "junji"
+#contraseña : "Tijunji2017"
+#cuenta local
+#nombre_usuario: "root"
+#contraseña: ""
